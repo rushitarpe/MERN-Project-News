21e86d9be36e1fe90ebdd909f135480523a0e50a
rushitarpe <rushitarpe44@gmail.com>
2025-01-05 18:29:22 +0530

    create a Middleware

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,14 +1,15 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req,res) => {
+export const signup = async (req,res,next) => {
     const { username,email,password } = req.body
     if(!username || 
         !email ||
          !password || 
          username === "" ||
         email === "" || password === "") {  
-        return res.status(400).json({message:"All fields are required"});
+           return next(errorHandler(400,"All fields are required"))
         }
         const hashedPassword=bcryptjs.hashSync(password,10)
         const newUser = new User({
@@ -21,6 +22,6 @@ try{
     res.json("Signup Successfull")
 }catch(error)
 {
-    res.status(500).json({message: error.message})
+   next(error)
 }
 }
--- Backend/index.js
+++ Backend/index.js
@@ -12,10 +12,20 @@ mongoose.connect(process.env.MONGO_URI)
 )
 const app = express();
 app.use(express.json());
-app.use("/api/auth",authRoutes)
+
 
 const port = 5000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}.!`);
 }
 )
+app.use("/api/auth",authRoutes)
+app.use((err,req,res,next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error!'
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
