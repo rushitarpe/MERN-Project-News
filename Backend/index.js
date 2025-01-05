b8c6fe0a6db2ceaf3a84e248863ad45568afee7a
rushitarpe <rushitarpe44@gmail.com>
2025-01-05 12:13:43 +0530

    Conncted to DataBase

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI)
+.then(() =>{ console.log("Database  Connected...")})
+.catch((err)=>{
+    console.log(err);
+}
+)
 const app = express();
 const port = 5000;
 app.listen(port,()=>{
